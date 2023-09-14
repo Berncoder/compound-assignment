615a103df718edd7a93f6c05bd4049bd4429f7e5
Bernadette <bernadettemetran35@gmail.com>
2023-09-13 22:56:05 -0700

    first commit

---
+++ compound assignment.js
@@ -0,0 +1,24 @@
+<!--Compound Assigment with Augmented Addition, Subtraction, Multiplication and Division -->
+
+<script>
+    let a = 3;
+    let b = 17;
+    let c = 12;
+
+    a += 12;
+    b += 9;
+    c += 7;
+    console.log(a, b, c); //output 15, 26, 19
+
+
+    a -= 6;
+    console.log(a); // 9
+
+    a *= 5;
+    console.log(a); // 15
+
+    a /= 3;
+    console.log(a); // 1
+
+
+</script>
